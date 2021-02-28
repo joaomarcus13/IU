a527076084396ba4b125ba1660a42c80508abca0
goncaloneto13 <g.neto13hotmail.com>
2021-02-27 21:29:50 -0300

    open img

--- frontend/whats/src/components/Login/Login.js
+++ frontend/whats/src/components/Login/Login.js
@@ -10,7 +10,7 @@ function Login() {
     const { setUser } = useContext(Context)
 
     const [codigo, setCodigo] = useState(false);
-    const [cadastro, setCadastro] = useState(false)
+    const [cadastro, setCadastro] = useState(true)
     const [userId, setUserId] = useState('')
     const [phone, setPhone] = useState('+11212345678')
 
@@ -92,6 +92,10 @@ function Login() {
         console.log('conta criada')
     }
 
+    function openImage() {
+      
+    }
+
     return (
         <div className='login'>
 
@@ -108,7 +112,8 @@ function Login() {
                         <h1>Cadastro</h1>
                         <div className='adft'>
                             <svg className='icon' xmlns="http://www.w3.org/2000/svg" viewBox="-5 -5 60 60" width="55" height="55"><defs><circle id="contact-SVGID_1_" cx="26.5" cy="26.5" r="25.5"></circle></defs><g clip-path="url(#contact-SVGID_2_)"><path fill="#0795DC" d="M26.5-1.1C11.9-1.1-1.1 5.6-1.1 27.6h55.2c-.1-19-13-28.7-27.6-28.7z"></path><path fill="#0EABF4" d="M53 26.5H-1.1c0 14.6 13 27.6 27.6 27.6s27.6-13 27.6-27.6H53z"></path></g><g fill="#F5F5F5"><path id="svg-contact" d="M26.5 26.5A4.5 4.5 0 0 0 31 22a4.5 4.5 0 0 0-4.5-4.5A4.5 4.5 0 0 0 22 22a4.5 4.5 0 0 0 4.5 4.5zm0 2.25c-3.004 0-9 1.508-9 4.5v1.125c0 .619.506 1.125 1.125 1.125h15.75c.619 0 1.125-.506 1.125-1.125V33.25c0-2.992-5.996-4.5-9-4.5z"></path></g></svg>
-                            <p>Adicionar foto ao perfil</p>
+                            <input type="file" id ="imgUser" ></input>
+        
                         </div>
                         <input type="text" placeholder='Nome' id='name' />
                         <input type="text" placeholder='Status' id='status' />
